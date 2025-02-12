0cb3a4c0548e70e4f046d070762fe98b2a21e4ef
ReiGoxheri <kingshow776@gmail.com>
2025-02-11 23:33:20 -0500

    Rewind

--- templates/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?> <!-- Loads the header -->
-
-<main class="error-404">
-    <h1>Page Not Found</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
-    <a href="<?php echo home_url(); ?>">Go to Homepage</a>
-</main>
-
-<?php get_footer(); ?> <!-- Loads the footer -->
--- templates/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?> <!-- Loads the header -->
-<main class="content">
-    <!-- wp:post-title /-->
-    <!-- wp:post-content /-->
-</main>
-<?php get_footer(); ?> <!-- Loads the footer -->
-
